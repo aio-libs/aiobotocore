401e88ee037dcc6e9948594837b781b3058f0eed
Nickolai Novik <nickolai@datarobot.com>
2015-06-01 14:11:27 +0300

    install content-type if not present

--- aiobotocore/endpoint.py
+++ aiobotocore/endpoint.py
@@ -86,8 +86,16 @@ class AioEndpoint(botocore.endpoint.Endpoint):
 
     @asyncio.coroutine
     def _send_request(self, request_dict, operation_model):
-        # TODO: fix this
-        request_dict['headers']['CONTENT-TYPE'] = 'application/octet-stream'
+
+        # install content-type header if not provided
+        headers = request_dict['headers']
+        for key in headers.keys():
+            if key.lower().startswith('content-type'):
+                break
+        else:
+            request_dict['headers']['Content-Type'] = \
+                'application/octet-stream'
+
         attempts = 1
         request = self.create_request(request_dict, operation_model)
         success_response, exception = yield from self._get_response(
