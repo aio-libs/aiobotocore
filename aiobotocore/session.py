46912a2b581c958b11c5ab464b3a72527030457f
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2016-03-25 14:33:25 -0700

    bugfix

--- aiobotocore/session.py
+++ aiobotocore/session.py
@@ -9,8 +9,9 @@ from .client import AioClientCreator
 class AioSession(botocore.session.Session):
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         self._loop = kwargs.pop('loop', None)
+
+        super().__init__(*args, **kwargs)
 
     def create_client(self, service_name, region_name=None, api_version=None,
                       use_ssl=True, verify=None, endpoint_url=None,
