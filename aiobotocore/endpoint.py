3fec11f4a929b54729a7f987f525a866f72f530c
Vitaliy Kharitonskyy <vharitonsky@gmail.com>
2016-12-20 11:37:17 +0200

    pass timeout to request, so it is propagated into StreamRead and affects .read method

--- aiobotocore/endpoint.py
+++ aiobotocore/endpoint.py
@@ -168,7 +168,8 @@ class AioEndpoint(Endpoint):
         headers_ = dict(
             (z[0], text_(z[1], encoding='utf-8')) for z in headers.items())
         request_coro = self._aio_session.request(method, url=url,
-                                                 headers=headers_, data=data)
+                                                 headers=headers_, data=data,
+                                                 timeout=self._read_timeout)
         resp = yield from asyncio.wait_for(
             request_coro, timeout=self._conn_timeout + self._read_timeout,
             loop=self._loop)
