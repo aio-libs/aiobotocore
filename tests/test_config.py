e0e1720a6f3e4dbf6d521d7bb78a7d3dca973150
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2017-03-07 13:37:51 -0800

    add more unittests to increase coverage

---
+++ tests/test_config.py
@@ -0,0 +1,39 @@
+from aiobotocore.config import AioConfig
+from botocore.config import Config
+from botocore.exceptions import ParamValidationError
+import pytest
+
+
+def test_connector_args():
+    with pytest.raises(ParamValidationError):
+        # wrong type
+        connector_args = dict(use_dns_cache=1)
+        AioConfig(connector_args)
+
+    with pytest.raises(ParamValidationError):
+        # wrong type
+        connector_args = dict(keepalive_timeout="1")
+        AioConfig(connector_args)
+
+    with pytest.raises(ParamValidationError):
+        # wrong type
+        connector_args = dict(force_close="1")
+        AioConfig(connector_args)
+
+    with pytest.raises(ParamValidationError):
+        # wrong type
+        connector_args = dict(ssl_context="1")
+        AioConfig(connector_args)
+
+    with pytest.raises(ParamValidationError):
+        # invalid key
+        connector_args = dict(foo="1")
+        AioConfig(connector_args)
+
+    # test merge
+    cfg = Config(read_timeout=75)
+    aio_cfg = AioConfig({'keepalive_timeout': 75})
+    aio_cfg.merge(cfg)
+
+    assert cfg.read_timeout == 75
+    assert aio_cfg.connector_args['keepalive_timeout'] == 75
