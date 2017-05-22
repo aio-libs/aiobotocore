ab3e5efd81207aa4519e34eeb6419e770a25982c
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2017-05-22 12:48:07 -0700

    add classifier for 3.6 support

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Environment :: Web Environment',
     'Development Status :: 3 - Alpha',
     'Framework :: AsyncIO',
