6854c50194f50bb11a59887c864c1319e7b0f884
Alexander Mohr <thehesiod@users.noreply.github.com>
2018-02-23 10:10:07 -0800

    fix connector timeout (#485)
    * fix connector timeout
    * add initial tox.ini for testing

--- tests/mock_server.py
+++ tests/mock_server.py
@@ -1,3 +1,6 @@
+import asyncio
+import aiohttp.web
+from aiohttp.web import StreamResponse
 import pytest
 import requests
 import shutil
@@ -5,6 +8,8 @@ import signal
 import subprocess as sp
 import sys
 import time
+import threading
+import socket
 
 
 _proxy_bypass = {
@@ -13,15 +18,98 @@ _proxy_bypass = {
 }
 
 
+host = "localhost"
+
+
+def get_free_tcp_port():
+    sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sckt.bind((host, 0))
+    addr, port = sckt.getsockname()
+    sckt.close()
+    return port
+
+
+class AIOServer(threading.Thread):
+    def __init__(self):
+        super().__init__(target=self._run)
+        self._loop = None
+        self._port = get_free_tcp_port()
+        self.start()
+        self.endpoint_url = 'http://{}:{}'.format(host, self._port)
+        self._shutdown_evt = threading.Event()
+
+    def _run(self):
+        self._loop = asyncio.new_event_loop()
+        app = aiohttp.web.Application(loop=self._loop)
+        app.router.add_route('*', '/ok', self.ok)
+        app.router.add_route('*', '/{anything:.*}', self.stream_handler)
+
+        try:
+            aiohttp.web.run_app(app, host=host, port=self._port,
+                                loop=self._loop, handle_signals=False)
+        except BaseException:
+            pytest.fail('unable to start and connect to aiohttp server')
+            raise
+        finally:
+            self._shutdown_evt.set()
+
+    @asyncio.coroutine
+    def ok(self, request):
+        return aiohttp.web.Response()
+
+    @asyncio.coroutine
+    def stream_handler(self, request):
+        # Without the Content-Type, most (all?) browsers will not render
+        # partially downloaded content. Note, the response type is
+        # StreamResponse not Response.
+        resp = StreamResponse(status=200, reason='OK',
+                              headers={'Content-Type': 'text/html'})
+
+        yield from resp.prepare(request)
+        yield from asyncio.sleep(5, loop=self._loop)
+        yield from resp.drain()
+        return resp
+
+    def wait_until_up(self):
+        connected = False
+        for i in range(0, 30):
+            try:
+                # we need to bypass the proxies due to monkey patches
+                requests.get(self.endpoint_url + '/ok', timeout=0.5,
+                             proxies=_proxy_bypass)
+                connected = True
+                break
+            except (requests.exceptions.ConnectionError,
+                    requests.exceptions.ReadTimeout):
+                time.sleep(0.5)
+            except BaseException:
+                pytest.fail('unable to start and connect to aiohttp server')
+                raise
+
+        if not connected:
+            pytest.fail('unable to start and connect to aiohttp server')
+
+    @asyncio.coroutine
+    def stop(self):
+        if self._loop:
+            self._loop.stop()
+
+            if not self._shutdown_evt.wait(20):
+                pytest.fail("Unable to shut down server")
+
+
 def start_service(service_name, host, port):
     moto_svr_path = shutil.which("moto_server")
     args = [sys.executable, moto_svr_path, service_name, "-H", host,
             "-p", str(port)]
-    process = sp.Popen(args, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.DEVNULL)
+    process = sp.Popen(args, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
     url = "http://{host}:{port}".format(host=host, port=port)
 
     for i in range(0, 30):
         if process.poll() is not None:
+            stdout, stderr = process.communicate()
+            pytest.fail("service failed starting up: {}  stdout: {} stderr: {}"
+                        "".format(service_name, stdout, stderr))
             break
 
         try:
@@ -56,8 +144,11 @@ def s3_server():
     port = 5000
     url = "http://{host}:{port}".format(host=host, port=port)
     process = start_service('s3', host, port)
-    yield url
-    stop_process(process)
+
+    try:
+        yield url
+    finally:
+        stop_process(process)
 
 
 @pytest.yield_fixture(scope="session")
@@ -66,8 +157,11 @@ def dynamodb2_server():
     port = 5001
     url = "http://{host}:{port}".format(host=host, port=port)
     process = start_service('dynamodb2', host, port)
-    yield url
-    stop_process(process)
+
+    try:
+        yield url
+    finally:
+        stop_process(process)
 
 
 @pytest.yield_fixture(scope="session")
@@ -76,8 +170,11 @@ def cloudformation_server():
     port = 5002
     url = "http://{host}:{port}".format(host=host, port=port)
     process = start_service('cloudformation', host, port)
-    yield url
-    stop_process(process)
+
+    try:
+        yield url
+    finally:
+        stop_process(process)
 
 
 @pytest.yield_fixture(scope="session")
@@ -86,8 +183,11 @@ def sns_server():
     port = 5003
     url = "http://{host}:{port}".format(host=host, port=port)
     process = start_service('sns', host, port)
-    yield url
-    stop_process(process)
+
+    try:
+        yield url
+    finally:
+        stop_process(process)
 
 
 @pytest.yield_fixture(scope="session")
@@ -96,5 +196,8 @@ def sqs_server():
     port = 5004
     url = "http://{host}:{port}".format(host=host, port=port)
     process = start_service('sqs', host, port)
-    yield url
-    stop_process(process)
+
+    try:
+        yield url
+    finally:
+        stop_process(process)
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,6 @@
+import asyncio
+from mock_server import AIOServer
+from aiobotocore.session import AioSession
 from aiobotocore.config import AioConfig
 from botocore.config import Config
 from botocore.exceptions import ParamValidationError
@@ -39,3 +42,42 @@ def test_connector_args():
 
     assert cfg.read_timeout == 75
     assert aio_cfg.connector_args['keepalive_timeout'] == 75
+
+
+@pytest.mark.moto
+@pytest.mark.run_loop
+def test_connector_timeout(loop):
+    server = AIOServer()
+    session = AioSession(loop=loop)
+    config = AioConfig(max_pool_connections=1, connect_timeout=1,
+                       retries={'max_attempts': 0})
+    s3_client = session.create_client('s3', config=config,
+                                      endpoint_url=server.endpoint_url,
+                                      aws_secret_access_key='xxx',
+                                      aws_access_key_id='xxx')
+
+    try:
+        server.wait_until_up()
+
+        @asyncio.coroutine
+        def get_and_wait():
+            yield from s3_client.get_object(Bucket='foo', Key='bar')
+            yield from asyncio.sleep(100)
+
+        # this should not raise as we won't have any issues connecting to the
+        task1 = asyncio.Task(get_and_wait(), loop=loop)
+        task2 = asyncio.Task(get_and_wait(), loop=loop)
+
+        try:
+            done, pending = yield from asyncio.wait([task1, task2],
+                                                    timeout=3, loop=loop)
+
+            # second request should not timeout just because there isn't a
+            # connector available
+            assert len(pending) == 2
+        finally:
+            task1.cancel()
+            task2.cancel()
+    finally:
+        s3_client.close()
+        yield from server.stop()
