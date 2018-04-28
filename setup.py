3d1c5162fa28740da2f0737a577b89abf2429e70
Alexander Mohr <thehesiod@hotmail.com>
2018-04-28 01:00:00 -0700

    pep!

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ from setuptools import setup, find_packages
 # If adding requirements make sure to also add to requirements-dev.txt
 install_requires = [
     'botocore>=1.8.0, <=1.8.21',
-    'aiohttp>=3.1.0, <3.2.0',  # for now we depend on internals of aiohttp so you can't keep this range open
+
+    # for now we depend on internals of aiohttp so this range can't be open
+    'aiohttp>=3.1.0, <3.2.0',
     'multidict>=2.1.4',
     'wrapt>=1.10.10',
     'packaging>=16.8',
