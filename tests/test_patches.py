2002808e5c65318d89aa3d6e0ae95153d8d0e517
Alexander Mohr <thehesiod@hotmail.com>
2018-04-30 19:33:51 -0700

    revert aiohttp 3.1.x change

--- tests/test_patches.py
+++ tests/test_patches.py
@@ -79,13 +79,6 @@ def test_patches():
 # NOTE: this doesn't require moto but needs to be marked to run with coverage
 @pytest.mark.moto
 def test_set_status_code(event_loop):
-    resp = ClientResponseProxy(
-        'GET', URL('http://foo/bar'),
-        writer=None, continue100=None, timer=None,
-        request_info=None,
-        auto_decompress=None,
-        traces=None,
-        loop=event_loop,
-        session=None)
+    resp = ClientResponseProxy('GET', URL('http://foo/bar'))
     resp.status_code = 500
     assert resp.status_code == 500
