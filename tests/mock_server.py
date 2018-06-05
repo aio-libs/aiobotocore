6af8d66c4f609835d9f320aff8eba063789900ea
Alexander Mohr <thehesiod@users.noreply.github.com>
2018-06-05 16:36:08 -0700

    switch to new aiohttp release (#596)

--- tests/mock_server.py
+++ tests/mock_server.py
@@ -4,7 +4,6 @@ import aiohttp.web
 from aiohttp.web import StreamResponse
 import pytest
 import requests
-import shutil
 import signal
 import subprocess as sp
 import sys
@@ -106,9 +105,8 @@ class AIOServer(multiprocessing.Process):
 
 
 def start_service(service_name, host, port):
-    moto_svr_path = shutil.which("moto_server")
-    args = [sys.executable, moto_svr_path, service_name, "-H", host,
-            "-p", str(port)]
+    args = [sys.executable, "-m", "moto.server", "-H", host,
+            "-p", str(port), service_name]
 
     # If test fails stdout/stderr will be shown
     process = sp.Popen(args, stdin=sp.PIPE)
