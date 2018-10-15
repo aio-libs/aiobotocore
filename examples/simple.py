76afb42dbca29e16ae6329fcd537920e3403ce76
SeungukLee <lsy931106@gmail.com>
2018-10-15 22:35:39 +0900

    Edit code (examples/simple.py)
    Signed-off-by: SeungukLee <lsy931106@gmail.com>

--- examples/simple.py
+++ examples/simple.py
@@ -13,23 +13,23 @@ async def go(loop):
     key = '{}/{}'.format(folder, filename)
 
     session = aiobotocore.get_session(loop=loop)
-    client = session.create_client('s3', region_name='us-west-2',
-                                   aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-                                   aws_access_key_id=AWS_ACCESS_KEY_ID)
-    # upload object to amazon s3
-    data = b'\x01'*1024
-    resp = await client.put_object(Bucket=bucket,
-                                   Key=key,
-                                   Body=data)
-    print(resp)
+    async with session.create_client('s3', region_name='us-west-2',
+                                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                                     aws_access_key_id=AWS_ACCESS_KEY_ID) as client:
+        # upload object to amazon s3
+        data = b'\x01' * 1024
+        resp = await client.put_object(Bucket=bucket,
+                                       Key=key,
+                                       Body=data)
+        print(resp)
 
-    # getting s3 object properties of file we just uploaded
-    resp = await client.get_object_acl(Bucket=bucket, Key=key)
-    print(resp)
+        # getting s3 object properties of file we just uploaded
+        resp = await client.get_object_acl(Bucket=bucket, Key=key)
+        print(resp)
 
-    # delete object from s3
-    resp = await client.delete_object(Bucket=bucket, Key=key)
-    print(resp)
+        # delete object from s3
+        resp = await client.delete_object(Bucket=bucket, Key=key)
+        print(resp)
 
 
 loop = asyncio.get_event_loop()
