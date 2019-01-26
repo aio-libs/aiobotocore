d90aedff9d69db48ee3cd4560dea9aa5d82cb58d
Chih-Hsuan Yen <yen@chyen.cc>
2019-01-26 18:53:24 +0800

    Support Python 3.7

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Environment :: Web Environment',
     'Development Status :: 3 - Alpha',
     'Framework :: AsyncIO',
