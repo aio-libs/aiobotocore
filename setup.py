a65b2b82f573d4bc53c06874e6ebbc882372be14
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2019-10-24 22:48:28 -0700

    add 3.8

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ classifiers = [
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Environment :: Web Environment',
     'Development Status :: 3 - Alpha',
     'Framework :: AsyncIO',
