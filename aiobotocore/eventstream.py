9c7a5b61b4ad9e46652abb4be0eb6c84d424bdba
Terry Cain <terry@terrys-home.co.uk>
2019-12-19 19:49:22 +0000

    Simplified AioEventStream with async_generator

--- aiobotocore/eventstream.py
+++ aiobotocore/eventstream.py
@@ -1,54 +1,25 @@
 from botocore.eventstream import EventStream, EventStreamBuffer
+from async_generator import async_generator, yield_
 
 
 class AioEventStream(EventStream):
-    def __init__(self, *args, **kwargs):
-        super(AioEventStream, self).__init__(*args, **kwargs)
-        self._buffer = EventStreamBuffer()
-
-    def _create_raw_event_generator(self):
-        return self._raw_stream.iter_chunks()
+    @async_generator
+    async def _create_raw_event_generator(self):
+        event_stream_buffer = EventStreamBuffer()
+        async for chunk, _ in self._raw_stream.iter_chunks():
+            event_stream_buffer.add_data(chunk)
+            for event in event_stream_buffer:
+                await yield_(event)
 
     def __iter__(self):
         raise NotImplementedError('Use async-for instead')
 
     def __aiter__(self):
-        return self
+        return self.__anext__()
 
+    @async_generator
     async def __anext__(self):
-        # Basically this gets chunks of data from a stream,
-        # pumps them into a buffer class that does some error
-        # checking, splitting and then returns results
-        while True:
-            try:
-                chunk = await self._event_generator.__anext__()
-            except StopAsyncIteration:
-                break
-            else:
-                self._buffer.add_data(chunk[0])
-
-            event = self._get_event_from_buffer()
-            if event:
-                return event
-            # If we have no event, there might be more data needed
-            # from the stream, so loop round and try again
-
-        # The stream has been read completely, but the buffer
-        # might still have events in it.
-        event = self._get_event_from_buffer()
-        if event:
-            return event
-
-        raise StopAsyncIteration()
-
-    def _get_event_from_buffer(self):
-        try:
-            while True:
-                event = next(self._buffer)
-                parsed_event = self._parse_event(event)
-                if parsed_event:
-                    return parsed_event
-        except StopIteration:
-            pass
-
-        return None
+        async for event in self._event_generator:
+            parsed_event = self._parse_event(event)
+            if parsed_event:
+                await yield_(parsed_event)
