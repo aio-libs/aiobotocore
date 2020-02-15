4848004cfb0c37ce41a0ec8851a4211eb0ced8e8
Alexander Mohr <thehesiod@users.noreply.github.com>
2020-02-15 02:37:16 -0800

    bump dev modules, speed up tests, fix tests

--- tests/mock_server.py
+++ tests/mock_server.py
@@ -1,31 +1,20 @@
 import asyncio
+import multiprocessing
+
+# Third Party
 import aiohttp
 import aiohttp.web
 from aiohttp.web import StreamResponse
 import pytest
-import requests
-import signal
-import subprocess as sp
-import sys
-import time
-import socket
-import multiprocessing
+
+# aiobotocore
+from tests.moto_server import host, MotoService, get_free_tcp_port
 
 
 _proxy_bypass = {
   "http": None,
   "https": None,
 }
-
-host = "localhost"
-
-
-def get_free_tcp_port():
-    sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sckt.bind((host, 0))
-    addr, port = sckt.getsockname()
-    sckt.close()
-    return port
 
 
 # This runs in a subprocess for a variety of reasons
@@ -40,7 +29,7 @@ class AIOServer(multiprocessing.Process):
     def __init__(self):
         super().__init__(target=self._run)
         self._loop = None
-        self._port = get_free_tcp_port()
+        self._port = get_free_tcp_port(True)
         self.endpoint_url = 'http://{}:{}'.format(host, self._port)
         self.daemon = True  # die when parent dies
 
@@ -69,7 +58,8 @@ class AIOServer(multiprocessing.Process):
             pytest.fail("Unable to shut down server")
             raise
 
-    async def ok(self, request):
+    @staticmethod
+    async def ok(request):
         return aiohttp.web.Response()
 
     async def stream_handler(self, request):
@@ -104,106 +94,31 @@ class AIOServer(multiprocessing.Process):
         pytest.fail('unable to start and connect to aiohttp server')
 
 
-def start_service(service_name, host, port):
-    args = [sys.executable, "-m", "moto.server", "-H", host,
-            "-p", str(port), service_name]
-
-    # If test fails stdout/stderr will be shown
-    process = sp.Popen(args, stdin=sp.PIPE)
-    url = "http://{host}:{port}".format(host=host, port=port)
-
-    for i in range(0, 30):
-        if process.poll() is not None:
-            process.communicate()
-            pytest.fail("service failed starting up: {}".format(service_name))
-            break
-
-        try:
-            # we need to bypass the proxies due to monkeypatches
-            requests.get(url, timeout=0.5, proxies=_proxy_bypass)
-            break
-        except requests.exceptions.ConnectionError:
-            time.sleep(0.5)
-    else:
-        stop_process(process)  # pytest.fail doesn't call stop_process
-        pytest.fail("Can not start service: {}".format(service_name))
-
-    return process
+@pytest.yield_fixture
+async def s3_server():
+    async with MotoService('s3') as svc:
+        yield svc.endpoint_url
 
 
-def stop_process(process):
-    try:
-        process.send_signal(signal.SIGTERM)
-        process.communicate(timeout=20)
-    except sp.TimeoutExpired:
-        process.kill()
-        outs, errors = process.communicate(timeout=20)
-        exit_code = process.returncode
-        msg = "Child process finished {} not in clean way: {} {}" \
-            .format(exit_code, outs, errors)
-        raise RuntimeError(msg)
+@pytest.yield_fixture
+async def dynamodb2_server():
+    async with MotoService('dynamodb2') as svc:
+        yield svc.endpoint_url
 
 
-@pytest.yield_fixture(scope="session")
-def s3_server():
-    host = "localhost"
-    port = 5000
-    url = "http://{host}:{port}".format(host=host, port=port)
-    process = start_service('s3', host, port)
-
-    try:
-        yield url
-    finally:
-        stop_process(process)
+@pytest.yield_fixture
+async def cloudformation_server():
+    async with MotoService('cloudformation') as svc:
+        yield svc.endpoint_url
 
 
-@pytest.yield_fixture(scope="session")
-def dynamodb2_server():
-    host = "localhost"
-    port = 5001
-    url = "http://{host}:{port}".format(host=host, port=port)
-    process = start_service('dynamodb2', host, port)
-
-    try:
-        yield url
-    finally:
-        stop_process(process)
+@pytest.yield_fixture
+async def sns_server():
+    async with MotoService('sns') as svc:
+        yield svc.endpoint_url
 
 
-@pytest.yield_fixture(scope="session")
-def cloudformation_server():
-    host = "localhost"
-    port = 5002
-    url = "http://{host}:{port}".format(host=host, port=port)
-    process = start_service('cloudformation', host, port)
-
-    try:
-        yield url
-    finally:
-        stop_process(process)
-
-
-@pytest.yield_fixture(scope="session")
-def sns_server():
-    host = "localhost"
-    port = 5003
-    url = "http://{host}:{port}".format(host=host, port=port)
-    process = start_service('sns', host, port)
-
-    try:
-        yield url
-    finally:
-        stop_process(process)
-
-
-@pytest.yield_fixture(scope="session")
-def sqs_server():
-    host = "localhost"
-    port = 5004
-    url = "http://{host}:{port}".format(host=host, port=port)
-    process = start_service('sqs', host, port)
-
-    try:
-        yield url
-    finally:
-        stop_process(process)
+@pytest.yield_fixture
+async def sqs_server():
+    async with MotoService('sqs') as svc:
+        yield svc.endpoint_url
