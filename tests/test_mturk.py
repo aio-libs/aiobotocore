1474acd5d3aebad197113fdb363ba6ea013183ce
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2020-06-03 17:58:31 -0700

    add test

--- tests/test_mturk.py
+++ tests/test_mturk.py
@@ -1,10 +1,34 @@
+from botocore.stub import Stubber, ANY
 import pytest
-import aiobotocore.session
 
 
-# @pytest.mark.moto  unfortunately moto doesn't support this yet :(
+_mturk_list_hits_response = {
+    'NumResults': 0,
+    'HITs': [],
+    'ResponseMetadata': {
+        'RequestId': '00000000-4989-4ffc-85cd-aaaaaaaaaaaa',
+        'HTTPStatusCode': 200,
+        'HTTPHeaders': {
+            'x-amzn-requestid': '00000000-4989-4ffc-85cd-aaaaaaaaaaaa',
+            'content-type': 'application/x-amz-json-1.1',
+            'content-length': '26',
+            'date': 'Thu, 04 Jun 2020 00:48:16 GMT'
+        },
+        'RetryAttempts': 0
+    }
+}
+
+
+# Unfortunately moto does not support mturk yet
+@pytest.mark.moto
 @pytest.mark.asyncio
-async def test_mturk():
-    session = aiobotocore.session.AioSession(profile='thehesiod')
-    async with session.create_client('mturk') as client:
-        await client.list_hi_ts_for_qualification_type(QualificationTypeId='string')
+async def test_mturk_stubber(session):
+    async with session.create_client('mturk', region_name='us-east-1') as client:
+        with Stubber(client) as stubber:
+            stubber.add_response('list_hits_for_qualification_type',
+                                 _mturk_list_hits_response,
+                                 {'QualificationTypeId': ANY})
+
+            response = await client.list_hi_ts_for_qualification_type(
+                QualificationTypeId='string')
+            assert response == _mturk_list_hits_response
