66fbeb9ecf8e538b2eb5ea620770c6f17d498eb8
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2020-06-03 17:26:19 -0700

    bugfix for #811

---
+++ tests/test_session.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.moto
+@pytest.mark.asyncio
+async def test_get_service_data(session):
+    handler_called = False
+
+    def handler(**kwargs):
+        nonlocal handler_called
+        handler_called = True
+
+    session.register('service-data-loaded.s3', handler)
+    response = await session.get_service_data('s3')
+
+    assert handler_called
