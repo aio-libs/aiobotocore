bddbeeec248ed25ae514c222f012e778fbd4655c
Alexander Mohr <thehesiod@users.noreply.github.com>
2020-08-18 02:59:34 -0700

    bump botocore (#823)

--- aiobotocore/__init__.py
+++ aiobotocore/__init__.py
@@ -1,4 +1,4 @@
 from .session import get_session, AioSession
 
 __all__ = ['get_session', 'AioSession']
-__version__ = '1.0.7'
+__version__ = '1.1.0'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 # NOTE: When updating botocore make sure to update awscli/boto3 versions below
 install_requires = [
     # pegged to also match items in `extras_require`
-    'botocore>=1.15.32,<1.15.33',
+    'botocore>=1.17.44,<1.17.45',
     'aiohttp>=3.3.1',
     'wrapt>=1.10.10',
     'aioitertools>=0.5.1',
@@ -19,8 +19,8 @@ def read(f):
 
 
 extras_require = {
-    'awscli': ['awscli==1.18.32'],
-    'boto3': ['boto3==1.12.32'],
+    'awscli': ['awscli==1.18.121'],
+    'boto3': ['boto3==1.14.44'],
 }
 
 
