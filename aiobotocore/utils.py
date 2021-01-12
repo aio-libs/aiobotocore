5431356ffc0ea623a5b79cd718bbec12e906044f
Alexander Mohr <thehesiod@gmail.com>
2021-01-12 02:50:38 -0800

    remove debug code

--- aiobotocore/utils.py
+++ aiobotocore/utils.py
@@ -113,9 +113,7 @@ class AioInstanceMetadataFetcher(AioIMDSFetcher, InstanceMetadataFetcher):
                          "attempting to retrieve data from metadata service.",
                          self._num_attempts)
         except BadIMDSRequestError as e:
-            import traceback
-            logger.debug("Bad IMDS request: %s exc: %s", e.request,
-                         traceback.format_exc())
+            logger.debug("Bad IMDS request: %s", e.request)
         return {}
 
     async def _get_iam_role(self, token=None):
