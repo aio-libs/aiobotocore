d55cc528fafe0f506b10e74fd945c17007774984
Alexander Mohr <thehesiod@users.noreply.github.com>
2021-11-02 09:54:07 -0700

    remove invalid entry

--- setup.py
+++ setup.py
@@ -45,7 +45,6 @@ classifiers = [
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10`',
     'Environment :: Web Environment',
     'Development Status :: 4 - Beta',
     'Framework :: AsyncIO',
