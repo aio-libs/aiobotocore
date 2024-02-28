188c721ae24d303ac35c5165425a29c95aaf0641
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2024-02-28 18:37:32 +0100

    Remove unused code

--- docs/conf.py
+++ docs/conf.py
@@ -123,10 +123,6 @@ html_theme_options = {
     'github_type': 'star',
     'github_banner': True,
 }
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
