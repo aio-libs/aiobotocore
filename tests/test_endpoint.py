6bdbf2fbfbd99a833ce4f4d10fa165c05d0111b8
Jakob Keller <57402305+jakob-keller@users.noreply.github.com>
2024-08-18 23:39:41 +0200

    test invalid `endpoint_url`

---
+++ tests/test_endpoint.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.mark.moto
+@pytest.mark.asyncio
+async def test_invalid_endpoint_url(session, region):
+    endpoint_url = 'invalid_url'
+    with pytest.raises(ValueError, match=f'Invalid endpoint: {endpoint_url}'):
+        async with session.create_client(
+            'ec2', region_name=region, endpoint_url=endpoint_url
+        ):
+            raise AssertionError  # should not succeed in entering client context
