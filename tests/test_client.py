2f5635d3be470209bded1cb15f04032b410a381a
Jakob Keller <57402305+jakob-keller@users.noreply.github.com>
2024-08-18 21:09:40 +0200

    test error condition of `client.get_paginator()`

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,12 @@
 import pytest
+from botocore.exceptions import OperationNotPageableError
+
+
+@pytest.mark.moto
+def test_get_paginator_not_supported_by_service(sns_client):
+    operation_name = 'list_tags_for_resource'
+    with pytest.raises(OperationNotPageableError):
+        sns_client.get_paginator(operation_name)
 
 
 @pytest.mark.moto
