b5d667b64a0b531ec07b2eba3119057f59fb69b9
Jakob Keller <57402305+jakob-keller@users.noreply.github.com>
2024-08-18 22:02:22 +0200

    test configuration defaults mode

---
+++ tests/test_configprovider.py
@@ -0,0 +1,20 @@
+import pytest
+
+from aiobotocore.session import AioSession
+
+
+@pytest.mark.parametrize(
+    'defaults_mode,retry_mode',
+    [('legacy', 'legacy'), ('standard', 'standard')],
+)
+@pytest.mark.moto
+@pytest.mark.asyncio
+async def test_defaults_mode(monkeypatch, defaults_mode, retry_mode):
+    monkeypatch.setenv('AWS_DEFAULTS_MODE', defaults_mode)
+
+    session = AioSession()
+
+    assert session.get_config_variable('defaults_mode') == defaults_mode
+
+    async with session.create_client('s3') as client:
+        assert client.meta.config.retries['mode'] == retry_mode
