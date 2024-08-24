90ff11a8a516a5d4c923788765ec72016771a027
Jakob Keller <57402305+jakob-keller@users.noreply.github.com>
2024-08-24 15:27:47 +0200

    set `default_role = 'any'` in Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -184,3 +184,5 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+
+default_role = 'any'
