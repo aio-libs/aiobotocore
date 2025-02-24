7ca46a3dba57b44280496d16e6839ca048bbcdd3
Alexander Mohr <thehesiod@users.noreply.github.com>
2025-02-24 14:16:05 -0800

    revert as master will no longer run under 3.8 on arm

--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -1,16 +1,11 @@
 import base64
 import io
 import json
-import os
 import zipfile
 
 # Third Party
 import botocore.client
 import pytest
-
-_MATRIX_CONTEXT = json.loads(os.environ.get('MATRIX_CONTEXT', '{}'))
-_MATRIX_OS = _MATRIX_CONTEXT.get('os')
-_MATRIX_PY_VERSION = _MATRIX_CONTEXT.get('python-version')
 
 
 async def _get_role_arn(iam_client, role_name: str):
@@ -47,12 +42,6 @@ def lambda_handler(event, context):
 
 
 async def test_run_lambda(request, iam_client, lambda_client, aws_lambda_zip):
-    # TODO: remove once moto 5.0.29 is released for python3.8 or we drop python3.8
-    #  support https://github.com/getmoto/moto/pull/8603
-    if _MATRIX_OS == 'ubuntu-24.04-arm' and _MATRIX_PY_VERSION == 3.8:
-        mark = pytest.mark.xfail(reason="xfail")  # pragma: no cover
-        request.node.add_marker(mark)  # pragma: no cover
-
     role_arn = await _get_role_arn(iam_client, 'test-iam-role')
     lambda_response = await lambda_client.create_function(
         FunctionName='test-function',
