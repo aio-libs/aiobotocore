2b1f846f034a36e4ac0200101b5fbd520b0b5000
Stefan T <66305+stj@users.noreply.github.com>
2025-04-16 15:02:22 -0700

    Use new classes to remove code duplication
    Having the `parse` method async on all parsers means
    the need for checking that `parse` is a coroutine is not needed
    anymore. This check moved into the parser where a private method
    can be async or sync. This cleans up the public interface of the
    AioParser classes.

--- aiobotocore/parsers.py
+++ aiobotocore/parsers.py
@@ -1,3 +1,5 @@
+import inspect
+
 from botocore.parsers import (
     LOG,
     EC2QueryParser,
@@ -32,6 +34,43 @@ class AioResponseParser(ResponseParser):
         name = response['context'].get('operation_name')
         return AioEventStream(response['body'], shape, parser, name)
 
+    # this is actually from ResponseParser and used by JSONParser and
+    # RpcV2CBORParser that need this async
+    async def parse(self, response, shape):
+        LOG.debug('Response headers: %s', response['headers'])
+        LOG.debug('Response body:\n%s', response['body'])
+        if response['status_code'] >= 301:
+            if self._is_generic_error_response(response):
+                parsed = self._do_generic_error_parse(response)
+            elif self._is_modeled_error_shape(shape):
+                parsed = self._do_modeled_error_parse(response, shape)
+                # We don't want to decorate the modeled fields with metadata
+                return parsed
+            else:
+                parsed = self._do_error_parse(response, shape)
+        else:
+            parsed = self._do_parse(response, shape)
+            if inspect.iscoroutine(parsed):
+                parsed = await parsed
+
+        # We don't want to decorate event stream responses with metadata
+        if shape and shape.serialization.get('eventstream'):
+            return parsed
+
+        # Add ResponseMetadata if it doesn't exist and inject the HTTP
+        # status code and headers from the response.
+        if isinstance(parsed, dict):
+            response_metadata = parsed.get('ResponseMetadata', {})
+            response_metadata['HTTPStatusCode'] = response['status_code']
+            # Ensure that the http header keys are all lower cased. Older
+            # versions of urllib3 (< 1.11) would unintentionally do this for us
+            # (see urllib3#633). We need to do this conversion manually now.
+            headers = response['headers']
+            response_metadata['HTTPHeaders'] = lowercase_dict(headers)
+            parsed['ResponseMetadata'] = response_metadata
+            self._add_checksum_response_metadata(response, response_metadata)
+        return parsed
+
 
 class AioQueryParser(QueryParser, AioResponseParser):
     pass
@@ -39,42 +78,6 @@ class AioQueryParser(QueryParser, AioResponseParser):
 
 class AioEC2QueryParser(EC2QueryParser, AioResponseParser):
     pass
-
-
-# this is actually from ResponseParser and used by JSONParser and
-# RpcV2CBORParser that need this async
-async def _parse(self, response, shape):
-    LOG.debug('Response headers: %s', response['headers'])
-    LOG.debug('Response body:\n%s', response['body'])
-    if response['status_code'] >= 301:
-        if self._is_generic_error_response(response):
-            parsed = self._do_generic_error_parse(response)
-        elif self._is_modeled_error_shape(shape):
-            parsed = self._do_modeled_error_parse(response, shape)
-            # We don't want to decorate the modeled fields with metadata
-            return parsed
-        else:
-            parsed = self._do_error_parse(response, shape)
-    else:
-        parsed = await self._do_parse(response, shape)
-
-    # We don't want to decorate event stream responses with metadata
-    if shape and shape.serialization.get('eventstream'):
-        return parsed
-
-    # Add ResponseMetadata if it doesn't exist and inject the HTTP
-    # status code and headers from the response.
-    if isinstance(parsed, dict):
-        response_metadata = parsed.get('ResponseMetadata', {})
-        response_metadata['HTTPStatusCode'] = response['status_code']
-        # Ensure that the http header keys are all lower cased. Older
-        # versions of urllib3 (< 1.11) would unintentionally do this for us
-        # (see urllib3#633). We need to do this conversion manually now.
-        headers = response['headers']
-        response_metadata['HTTPHeaders'] = lowercase_dict(headers)
-        parsed['ResponseMetadata'] = response_metadata
-        self._add_checksum_response_metadata(response, response_metadata)
-    return parsed
 
 
 class AioJSONParser(JSONParser, AioResponseParser):
@@ -103,8 +106,6 @@ class AioJSONParser(JSONParser, AioResponseParser):
         parsed[event_name] = event_stream
         return parsed
 
-    parse = _parse
-
 
 class AioRestJSONParser(RestJSONParser, AioResponseParser):
     pass
@@ -114,12 +115,7 @@ class AioRestXMLParser(RestXMLParser, AioResponseParser):
     pass
 
 
-class AioRpcV2CBORParser(RpcV2CBORParser):
-    def _create_event_stream(self, response, shape):
-        parser = self._event_stream_parser
-        name = response['context'].get('operation_name')
-        return AioEventStream(response['body'], shape, parser, name)
-
+class AioRpcV2CBORParser(RpcV2CBORParser, AioResponseParser):
     async def _do_parse(self, response, shape):
         parsed = {}
         if shape is not None:
@@ -147,8 +143,6 @@ class AioRpcV2CBORParser(RpcV2CBORParser):
         parsed = self._initial_body_parse(event.payload)
         parsed[event_name] = event_stream
         return parsed
-
-    parse = _parse
 
 
 PROTOCOL_PARSERS = {
