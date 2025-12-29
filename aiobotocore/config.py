c05554d2673ba2b812476989acea863cb750a530
Jakob Keller <57402305+jakob-keller@users.noreply.github.com>
2025-12-29 12:59:52 +0100

    Add partial type hints to `AioConfig`

--- aiobotocore/config.py
+++ aiobotocore/config.py
@@ -1,31 +1,57 @@
 import copy
+import ssl
+import sys
+from typing import Optional, TypedDict, Union
 
 import botocore.client
+from aiohttp.abc import AbstractResolver
 from botocore.exceptions import ParamValidationError
 
 from ._constants import DEFAULT_KEEPALIVE_TIMEOUT
 from .endpoint import DEFAULT_HTTP_SESSION_CLS
+from .httpsession import AIOHTTPSession
 from .httpxsession import HttpxSession
+
+if sys.version_info >= (3, 11):
+    from typing import NotRequired
+else:
+    from typing_extensions import NotRequired
 
 TIMEOUT_ARGS = frozenset(
     ('keepalive_timeout', 'write_timeout', 'pool_timeout')
 )
 
 
+class _ConnectorArgs(TypedDict):
+    use_dns_cache: NotRequired[bool]
+    ttl_dns_cache: NotRequired[Optional[int]]
+    keepalive_timeout: NotRequired[Optional[float]]
+    write_timeout: NotRequired[Optional[float]]
+    pool_timeout: NotRequired[Optional[float]]
+    force_close: NotRequired[bool]
+    ssl_context: NotRequired[ssl.SSLContext]
+    resolver: NotRequired[AbstractResolver]
+
+
+_HttpSessionType = Union[AIOHTTPSession, HttpxSession]
+
+
 class AioConfig(botocore.client.Config):
     def __init__(
         self,
-        connector_args=None,
-        http_session_cls=DEFAULT_HTTP_SESSION_CLS,
+        connector_args: Optional[_ConnectorArgs] = None,
+        http_session_cls: type[_HttpSessionType] = DEFAULT_HTTP_SESSION_CLS,
         **kwargs,
     ):
         super().__init__(**kwargs)
 
-        self._validate_connector_args(connector_args, http_session_cls)
-        self.connector_args = copy.copy(connector_args)
-        self.http_session_cls = http_session_cls
-        if not self.connector_args:
-            self.connector_args = dict()
+        self.connector_args: _ConnectorArgs = (
+            copy.copy(connector_args) if connector_args else {}
+        )
+        self.http_session_cls: type[_HttpSessionType] = http_session_cls
+        self._validate_connector_args(
+            self.connector_args, self.http_session_cls
+        )
 
         if 'keepalive_timeout' not in self.connector_args:
             self.connector_args['keepalive_timeout'] = (
@@ -39,10 +65,10 @@ class AioConfig(botocore.client.Config):
         return AioConfig(self.connector_args, **config_options)
 
     @staticmethod
-    def _validate_connector_args(connector_args, http_session_cls):
-        if connector_args is None:
-            return
-
+    def _validate_connector_args(
+        connector_args: _ConnectorArgs,
+        http_session_cls: type[_HttpSessionType],
+    ) -> None:
         for k, v in connector_args.items():
             # verify_ssl is handled by verify parameter to create_client
             if k == 'use_dns_cache':
@@ -75,15 +101,11 @@ class AioConfig(botocore.client.Config):
                     )
             # limit is handled by max_pool_connections
             elif k == 'ssl_context':
-                import ssl
-
                 if not isinstance(v, ssl.SSLContext):
                     raise ParamValidationError(
                         report=f'{k} must be an SSLContext instance'
                     )
             elif k == "resolver":
-                from aiohttp.abc import AbstractResolver
-
                 if http_session_cls is HttpxSession:
                     raise ParamValidationError(
                         report=f'Httpx backend does not support {k}.'
